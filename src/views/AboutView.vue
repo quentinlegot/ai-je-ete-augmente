dee39ee261ce34c2d27adbb8e4043ab7149df87d
Lars-VanCiental <services.bitbucket@valentin.claras.fr>
2023-07-07 18:42:22 +0200

    feat: add about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,54 @@
 <template>
   <main class="mx-auto max-w-screen-lg p-5">
     <h2 class="my-5 text-lg text-slate-800">A propos</h2>
+    <p class="mb-3 p-3">
+      Au cours de l'année 2022, on a pu voir les taux d'inflation s'envoler en france. Le question
+      s'est posée pour beaucoup de savoir si les "augmentations" annuelles allaient au moins couvrir
+      la hausse des prix.
+    </p>
+    <p class="mb-3 p-3">
+      L'interrogation s'est aussi faites pour les augmentations des années précédentes. A priori, on
+      ne pense pas à suivre l'inflation régulièrement.
+    </p>
+    <h2 class="my-5 text-lg text-slate-800">Objectif</h2>
+    <p class="mb-3 p-3">
+      Ce projet est né de la volonté de déterminer le % d'augmentation réelle, par rapport à la
+      perte de salaire suite à l'inflation, depuis la précédente augmentation.
+    </p>
+    <p class="mb-3 p-3">
+      Si vous souhaitez aller plus loin sur le sujet de l'inflation, il existe de multiples sources
+      bien plus fournies et sérieuses.
+    </p>
+    <ul class="mb-3 p-3">
+      <li>
+        <a
+          href="https://www.economie.gouv.fr/cedef/evolution-prix-consommation"
+          target="_blank"
+          class="underline"
+        >
+          Le site du gouvernement
+        </a>
+      </li>
+      <li>
+        <a href="https://www.insee.fr/fr/statistiques/4268033" target="_blank" class="underline">
+          L'insee
+        </a>
+      </li>
+      <li>
+        <a href="https://france-inflation.com" target="_blank" class="underline">
+          https://france-inflation.com
+        </a>
+      </li>
+    </ul>
+    <h2 class="my-5 text-lg text-slate-800">Contribuer</h2>
+    <p class="mb-3 p-3">
+      Si vous souhaitez participer,
+      <a
+        href="https://bitbucket.org/Lars-VanCiental/ai-je-ete-augmente/src/master/"
+        target="_blank"
+        class="underline"
+        >les sources sont disponibles ici</a
+      >.
+    </p>
   </main>
 </template>
